adb7cfb7d54956799ce4b466a4667c487e2e4ec7
Julian Andrade <julian-andrade@hotmail.com>
2024-02-23 10:58:22 -0300

    feat: add CreateUserUseCase

---
+++ src/db/use-cases/create-user.js
@@ -0,0 +1,29 @@
+import { v4 as uuidv4 } from 'uuid'
+import bcrypt from 'bcrypt'
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js'
+
+export class CreateUserUseCase {
+    async execute(createUserParams) {
+        // Verificar se o e-mail já está em uso
+
+        // Gerar o id do usuário
+        const userId = uuidv4()
+
+        // Criptografar a senha
+        const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
+
+        // Inserior o usuário no banco de dados
+        const user = {
+            ...createUserParams,
+            id: userId,
+            password: hashedPassword,
+        }
+
+        // Chamar o repositório
+        const postgresCreateUserRepository = new PostgresCreateUserRepository()
+
+        const createdUser = await postgresCreateUserRepository.execute(user)
+
+        return createdUser
+    }
+}
