0e77daa2ee1ea0f5e97ca6364513e5e5cdacc316
Julian Andrade <julian-andrade@hotmail.com>
2024-03-13 21:15:47 -0300

    feat: add zod to CreateUserController

--- src/controllers/user/create-user.js
+++ src/controllers/user/create-user.js
@@ -1,15 +1,7 @@
 import { EmailAlreadyInUseError } from '../../errors/user.js'
-import {
-    badRequest,
-    created,
-    serverError,
-    invalidPasswordResponse,
-    EmailIsAlreadyInUseResponse,
-    requiredFieldIsMissingResponse,
-    checkIfPasswordIsValid,
-    checkIfEmailIsValid,
-    validateRequiredFields,
-} from '../helpers/index.js'
+import { badRequest, created, serverError } from '../helpers/index.js'
+import { createUserSchema } from '../../schemas/user.js'
+import { ZodError } from 'zod'
 
 export class CreateUserController {
     constructor(createUserUseCase) {
@@ -18,32 +10,8 @@ export class CreateUserController {
     async execute(httpRequest) {
         try {
             const params = httpRequest.body
-            // Validar a requisição (campos obrigatórios, email e senha)
-            const requiredFields = [
-                'first_name',
-                'last_name',
-                'email',
-                'password',
-            ]
 
-            const { ok: requireFieldsWereProvided, missingField } =
-                validateRequiredFields(params, requiredFields)
-
-            if (!requireFieldsWereProvided) {
-                return requiredFieldIsMissingResponse(missingField)
-            }
-
-            const passwordIsValid = checkIfPasswordIsValid(params.password)
-
-            if (!passwordIsValid) {
-                return invalidPasswordResponse()
-            }
-
-            const emailIsValid = checkIfEmailIsValid(params.email)
-
-            if (!emailIsValid) {
-                return EmailIsAlreadyInUseResponse()
-            }
+            await createUserSchema.parseAsync(params)
 
             // Chamar o useCase
             const createdUser = await this.createUserUseCase.execute(params)
@@ -51,6 +19,12 @@ export class CreateUserController {
             // Retornar a resposta para o usuário (status code)
             return created(createdUser)
         } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                })
+            }
+
             if (error instanceof EmailAlreadyInUseError) {
                 return badRequest({ message: error.message })
             }
---
+++ src/schemas/user.js
@@ -0,0 +1,39 @@
+import { z } from 'zod'
+
+export const createUserSchema = z.object({
+    first_name: z
+        .string({
+            required_error: 'First name is required.',
+        })
+        .trim()
+        .min(1, {
+            message: 'Please provide a valid first name',
+        }),
+    last_name: z
+        .string({
+            required_error: 'Last name is required.',
+        })
+        .trim()
+        .min(1, {
+            message: 'Please provide a valid last name',
+        }),
+    email: z
+        .string({
+            required_error: 'Email is required.',
+        })
+        .email({
+            message: 'Please provide a valid email address',
+        })
+        .trim()
+        .min(1, {
+            message: 'Please provide a valid email address',
+        }),
+    password: z
+        .string({
+            required_error: 'Password is required.',
+        })
+        .trim()
+        .min(6, {
+            message: 'Password must be at least 6 characters',
+        }),
+})
