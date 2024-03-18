7b9968dcb14f8c207e5499db060c991bec094752
Julian Andrade <julian-andrade@hotmail.com>
2024-03-18 18:41:17 -0300

    feat: add Prisma to UpdateTransactionRepository and GetTransactionsByUserIdRepository

--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -1,29 +1,10 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresUpdateTransactionRepository {
     async execute(transactionId, updateTransactionParams) {
-        const updateFields = []
-        const updateValues = []
-
-        Object.keys(updateTransactionParams).forEach((key) => {
-            updateFields.push(`${key} = $${updateValues.length + 1}`)
-            updateValues.push(updateTransactionParams[key])
+        return await prisma.transaction.update({
+            where: { id: transactionId },
+            data: updateTransactionParams,
         })
-
-        updateValues.push(transactionId)
-
-        const updateQuery = `
-          UPDATE transactions
-          SET ${updateFields.join(', ')}
-          WHERE id = $${updateValues.length}
-          RETURNING *
-        `
-
-        const updateTransaction = await PostgresHelper.query(
-            updateQuery,
-            updateValues,
-        )
-
-        return updateTransaction[0]
     }
 }
