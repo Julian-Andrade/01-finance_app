5a3caac53a8da40bd6fa48525240368470b31aa9
Julian Andrade <julian-andrade@hotmail.com>
2024-03-21 10:26:27 -0300

    feat: ensure 400 is returned if first_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -1,17 +1,16 @@
 import { CreateUserController } from '../index.js'
 
 describe('Create User Controller', () => {
-    it('should return 201 when creating a new user', async () => {
-        class CreateUserUseCaseStub {
-            execute(user) {
-                return user
-            }
+    class CreateUserUseCaseStub {
+        execute(user) {
+            return user
         }
+    }
 
+    it('should return 201 when creating a new user', async () => {
         // Arrange
-        const createUserController = new CreateUserController(
-            new CreateUserUseCaseStub(),
-        )
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
 
         const httpRequest = {
             body: {
@@ -28,5 +27,25 @@ describe('Create User Controller', () => {
         // Assert
         expect(result.statusCode).toBe(201)
         expect(result.body).toBe(httpRequest.body)
+    })
+
+    it('should return 400 if first_name is not provided', async () => {
+        // Arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                last_name: 'Andrade',
+                email: 'julian@andrade.com',
+                password: '123456',
+            },
+        }
+
+        // Action
+        const result = await createUserController.execute(httpRequest)
+
+        // Assert
+        expect(result.statusCode).toBe(400)
     })
 })
