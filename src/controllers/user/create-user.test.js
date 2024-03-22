df44b9e8fb02cf7a66fac68215f12f4516208029
Julian Andrade <julian-andrade@hotmail.com>
2024-03-22 10:12:54 -0300

    test: ensure CreateUserController returns 500 if CreateUserUseCase throws

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -189,4 +189,31 @@ describe('Create User Controller', () => {
         expect(executeSpy).toHaveBeenCalledWith(httpRequest.body)
         expect(executeSpy).toHaveBeenCalledTimes(1)
     })
+
+    it('should return 500 if CreateUserUseCase throws', async () => {
+        // Arrange
+        const createUserUseCase = new CreateUserUseCaseStub()
+        const createUserController = new CreateUserController(createUserUseCase)
+
+        const httpRequest = {
+            body: {
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({
+                    length: 7,
+                }),
+            },
+        }
+
+        jest.spyOn(createUserUseCase, 'execute').mockImplementationOnce(() => {
+            throw new Error()
+        })
+
+        // Action
+        const result = await createUserController.execute(httpRequest)
+
+        // Assert
+        expect(result.statusCode).toBe(500)
+    })
 })
