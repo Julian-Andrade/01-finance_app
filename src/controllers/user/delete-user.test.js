f73f313b6066e812f9b16d965c23fdd4d5e905c7
Julian Andrade <julian-andrade@hotmail.com>
2024-03-25 10:33:27 -0300

    test: ensure DeleteUserController returns 404 if user is not found

--- src/controllers/user/delete-user.test.js
+++ src/controllers/user/delete-user.test.js
@@ -50,4 +50,16 @@ describe('Delete User Controller', () => {
         // Assert
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 404 if user is not found', async () => {
+        // Arrange
+        const { sut, deleteUserUseCase } = makeSut()
+        jest.spyOn(deleteUserUseCase, 'execute').mockReturnValueOnce(null)
+
+        // Action
+        const result = await sut.execute(httpRequest)
+
+        // Assert
+        expect(result.statusCode).toBe(404)
+    })
 })
