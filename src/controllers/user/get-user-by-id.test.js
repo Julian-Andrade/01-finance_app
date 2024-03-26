10a9f0dd59bd69eeb7a308961e63b714db03c89e
Julian Andrade <julian-andrade@hotmail.com>
2024-03-26 11:45:54 -0300

    test: ensure GetUserByIdController returns 200/400/404/500 when make all tests

---
+++ src/controllers/user/get-user-by-id.test.js
@@ -0,0 +1,79 @@
+import { faker } from '@faker-js/faker'
+import { GetUserByIdController } from '../index.js'
+
+describe('GetUserByIdController', () => {
+    class GetUserByIdUseCaseStub {
+        execute() {
+            return {
+                id: faker.string.uuid(),
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({
+                    length: 7,
+                }),
+            }
+        }
+    }
+
+    const makeSut = () => {
+        const getUserByIdUseCase = new GetUserByIdUseCaseStub()
+        const sut = new GetUserByIdController(getUserByIdUseCase)
+
+        return { getUserByIdUseCase, sut }
+    }
+
+    const httpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+    }
+
+    it('should return 200 when getting a user by id', async () => {
+        // Arrange
+        const { sut } = makeSut()
+
+        // Action
+        const result = await sut.execute(httpRequest)
+
+        // Assert
+        expect(result.statusCode).toBe(200)
+    })
+
+    it('should return 400 if userid is invalid', async () => {
+        // Arrange
+        const { sut } = makeSut()
+
+        // Action
+        const result = await sut.execute({ params: { userId: 'invalid_id' } })
+
+        // Assert
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 404 if user is not found', async () => {
+        // Arrange
+        const { sut, getUserByIdUseCase } = makeSut()
+        jest.spyOn(getUserByIdUseCase, 'execute').mockResolvedValue(null)
+
+        // Action
+        const result = await sut.execute(httpRequest)
+
+        // Assert
+        expect(result.statusCode).toBe(404)
+    })
+
+    it('should return 500 if GetUserByIdController throws', async () => {
+        // Arrange
+        const { sut, getUserByIdUseCase } = makeSut()
+        jest.spyOn(getUserByIdUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        // Action
+        const result = await sut.execute(httpRequest)
+
+        // Assert
+        expect(result.statusCode).toBe(500)
+    })
+})
