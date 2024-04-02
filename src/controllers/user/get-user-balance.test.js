7c4069d87f18bf5369667dd064e2a88784c8f595
Julian Andrade <julian-andrade@hotmail.com>
2024-04-02 10:03:58 -0300

    test: ensure GetUserBalanceController calls with GetUserBalanceUseCase with correct params

--- src/controllers/user/get-user-balance.test.js
+++ src/controllers/user/get-user-balance.test.js
@@ -56,4 +56,22 @@ describe('GetUserBalanceController', () => {
         // Assert
         expect(result.statusCode).toBe(500)
     })
+
+    it('should call GetUserBalanceUseCase with correct params ', async () => {
+        // Arrange
+        const { sut, getUserBalanceUseCase } = makeSut()
+        const executeSpy = jest.spyOn(getUserBalanceUseCase, 'execute')
+
+        const userId = faker.string.uuid()
+
+        // Action
+        await sut.execute({
+            params: {
+                userId,
+            },
+        })
+
+        // Assert
+        expect(executeSpy).toHaveBeenCalledWith(userId)
+    })
 })
