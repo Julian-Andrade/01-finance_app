61c279413099702062cf83838b1c562032183718
Julian Andrade <julian-andrade@hotmail.com>
2024-04-02 08:57:12 -0300

    test: ensure DeleteTransactionUseCase calls DeleteTransactionUseCase with correct params

--- src/controllers/transaction/delete-transaction.test.js
+++ src/controllers/transaction/delete-transaction.test.js
@@ -76,4 +76,22 @@ describe('DeleteTransactionController', () => {
         // Assert
         expect(result.statusCode).toBe(500)
     })
+
+    it('should call DeleteTransactionUseCase with correct params ', async () => {
+        // Arrange
+        const { sut, deleteTransactionUseCase } = makeSut()
+        const executeSpy = jest.spyOn(deleteTransactionUseCase, 'execute')
+
+        const transactionId = faker.string.uuid()
+
+        // Action
+        await sut.execute({
+            params: {
+                transactionId,
+            },
+        })
+
+        // Assert
+        expect(executeSpy).toHaveBeenCalledWith(transactionId)
+    })
 })
