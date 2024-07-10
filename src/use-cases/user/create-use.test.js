b83d1cef2ea2c722aa2d2c5becb6592e713df296
Julian Andrade Silva <julian-andrade@hotmail.com>
2024-07-10 09:44:17 -0300

    test: ensure CreateUserUseCase creates a user

---
+++ src/use-cases/user/create-use.test.js
@@ -0,0 +1,68 @@
+import { CreateUserUseCase } from './create-user'
+import { faker } from '@faker-js/faker'
+
+describe('Create User Use Case', () => {
+    class GetUserByEmailRepositoryStub {
+        async execute() {
+            return null
+        }
+    }
+
+    class CreateUserRepositoryStub {
+        async execute(user) {
+            return user
+        }
+    }
+
+    class PasswordHasherAdapterStub {
+        async execute() {
+            return 'hashed_password'
+        }
+    }
+
+    class IdGeneratorAdapterStub {
+        async execute() {
+            return 'generated_id'
+        }
+    }
+
+    const makeSut = () => {
+        const getUserByEmailRepository = new GetUserByEmailRepositoryStub()
+        const createUserRepository = new CreateUserRepositoryStub()
+        const passwordHashedAdapter = new PasswordHasherAdapterStub()
+        const idGeneratorAdapter = new IdGeneratorAdapterStub()
+
+        const sut = new CreateUserUseCase(
+            getUserByEmailRepository,
+            createUserRepository,
+            passwordHashedAdapter,
+            idGeneratorAdapter,
+        )
+
+        return {
+            sut,
+            getUserByEmailRepository,
+            createUserRepository,
+            passwordHashedAdapter,
+            idGeneratorAdapter,
+        }
+    }
+
+    it('should successfully create a user', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const createdUser = await sut.execute({
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password({
+                length: 7,
+            }),
+        })
+
+        // assert
+        expect(createdUser).toBeTruthy()
+    })
+})
